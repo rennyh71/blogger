e35a4f974a0efe66cf0ca2027d00bba8e1155518
Renny <renny@localhost.localdomain>
2024-06-07 13:05:19 -0400

    first commit

---
+++ src/components/frame.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Frame = () => {
+    return (
+        <div>
+            <h1>holaaaa</h1>
+            <button onClick={() => { alert('holfunciona') }}>
+                click
+            </button>
+        </div>
+
+    );
+}
+
+export default Frame;
